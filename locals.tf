f1423080ae2a81f36cfb8ef0d1e6180aec92045f
Hugo Thomaz <76590460+hugothomazpsouza@users.noreply.github.com>
2023-02-13 11:13:33 -0300

    Delete locals.tf

--- locals.tf
+++
@@ -1,4 +0,0 @@
-# https://stackoverflow.com/questions/46653464/terraform-how-can-i-read-variables-into-terraform-from-a-yaml-file-or-from-a-d
-locals {
-  config  = yamldecode(file("variables.yaml"))
-}
