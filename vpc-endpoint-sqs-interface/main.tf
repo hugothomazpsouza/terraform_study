1844f8396abcff7912c54055a71aec4db7a539a4
Hugo Thomaz <hugothomazpinheiro@gmail.com>
2023-06-11 20:50:34 -0300

    adding the vpc endpoint interface for sqs

--- vpc-endpoint-sqs-interface/main.tf
+++ vpc-endpoint-sqs-interface/main.tf
@@ -214,3 +214,29 @@ resource "aws_iam_instance_profile" "demo-profile" {
 #   service_name      = "com.amazonaws.us-east-1.s3"
 #   route_table_ids   = module.vpc.private_route_table_ids
 # }
+
+#sqs QUEUE - https://www.howtoforge.com/how-to-create-an-sqs-queue-on-aws-using-terraform/
+
+# resource "aws_sqs_queue" "my_first_sqs" {
+#   name = var.sqs_name
+# }
+
+# resource "aws_sqs_queue_policy" "my_sqs_policy" {
+#   queue_url = aws_sqs_queue.my_first_sqs.id
+
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Id": "sqspolicy",
+#   "Statement": [
+#     {
+#       "Sid": "First",
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": "sqs:SendMessage",
+#       "Resource": "${aws_sqs_queue.my_first_sqs.arn}"
+#     }
+#   ]
+# }
+# POLICY
+# }
